4a9a863767870b16d2844cfdbea42e412a0088cf
Sorin Florea <srnflr@gmail.com>
2025-02-13 00:12:49 +0100

    Commit - WHERE job_title_short

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -18,7 +18,7 @@ WITH top_paying_jobs AS (
         job_postings_fact
     LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE
-        job_title = 'Data Analyst' AND job_location = 'Anywhere'
+        job_title_short = 'Data Analyst' AND job_location = 'Anywhere'
         AND salary_year_avg IS NOT NULL 
     ORDER BY salary_year_avg DESC
     LIMIT 10
@@ -29,5 +29,4 @@ SELECT top_paying_jobs.*,
 FROM top_paying_jobs
 INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-ORDER BY salary_year_avg DESC
-
+ORDER BY salary_year_avg DESC;
